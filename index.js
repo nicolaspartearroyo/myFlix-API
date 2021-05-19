e59eb80bbaf1d375fa7644d19d7d53ba9995e37d
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-05-19 17:41:49 +0200

    username updated

--- index.js
+++ index.js
@@ -115,10 +115,15 @@ app.post('/users', (req, res) => {
   res.status(201).send('User successfully created');
 });
 
+student = username
+students = users
 //Updates user information and favourites movies. Not sure how to work with PUT!!!
 app.put('/users/:username', (req, res) => {
+  let username = users.find((username) => { return username.name === req.params.name });
+  username.classes[req.params.class] = parseInt(req.params.grade);
   res.status(201).send('New information was successfully updated');
 });
+
 
 app.put('/users/:username/favourites', (req, res) => {
   res.status(201).send('Favourite movie was successfully updated');
