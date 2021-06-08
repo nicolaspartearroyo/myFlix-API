8d62ece475f394e623dbd04515a5c8712cbf7e19
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-06-08 13:07:50 +0200

    error fixed on bcryot callback

--- models.js
+++ models.js
@@ -20,11 +20,11 @@ let userSchema = mongoose.Schema({
 });
 
 userSchema.statics.hashPassword = (password) => {
-    return.bcrypt.hashSync(password, 10);
+    return bcrypt.hashSync(password, 10);
 };
 
 userSchema.methods.validatePassword = function (password) {
-    return.bcrypt.compareSync(password, this.Password);
+    return bcrypt.compareSync(password, this.Password);
 };
 
 let directorSchema = mongoose.Schema({
