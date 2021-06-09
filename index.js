083f09a06fa0e2e6d3dc89bb0b8b9f8f040fac35
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-06-09 09:12:41 +0200

    mongoose server connection + data encrypt

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const Users = Models.User;
 const Directors = Models.Director;
 const Genres = Models.Genre;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const bodyParser = require('body-parser');
 const express = require('express'),
