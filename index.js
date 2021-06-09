c672cc083eac352fc62fd8b7956bdee803289606
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-06-09 10:42:45 +0200

    error handling out

--- index.js
+++ index.js
@@ -255,11 +255,11 @@ app.get('/genres/:Name', (req, res) => {
 app.use(express.static('public'));
 app.use(morgan('common'));
 
- //error handling
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
+//error handling
+//  app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send('Something broke!');
+// });
 
 // listen for requests
 const port = process.env.PORT || 8080;
