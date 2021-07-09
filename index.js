8d4d705b9f9ea147bc53abc40c2c797b59c4bfe1
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-07-09 14:13:41 +0200

    passport back

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/', (req, res) => {
 
 //// GET all movies
 // app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) => {
-app.get("/movies", function (req, res) {
+app.get("/movies", passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
