bc729a42fe028d617da8d9489d4c318b050233b3
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-07-09 14:42:08 +0200

    allowedOirigins localhost updated

--- index.js
+++ index.js
@@ -17,7 +17,20 @@ const Genres = Models.Genre;
 app = express();
 app.use(bodyParser.json());
 //Allow all domains to make request to my API
-app.use(cors());
+// app.use(cors());
+
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflixbypartearroyo.herokuapp.com/'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth')(app);
 
