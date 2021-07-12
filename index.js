fb0fad3f6396bb6486169b323e88b1b5790e5a4d
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-07-12 18:54:56 +0200

    cors updated

--- index.js
+++ index.js
@@ -21,18 +21,20 @@ app.use(bodyParser.json());
 
 let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflixbypartearroyo.herokuapp.com/'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors(
+  // {
+  // origin: (origin, callback) => {
+  //   if (!origin) return callback(null, true);
+  //   if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+  //     let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+  //     return callback(new Error(message), false);
+  //   }
+  //   return callback(null, true);
+  // }
+  // }
+));
 
-let auth = require('./auth')(app);
+let auth = require('./auth.js')(app);
 
 // mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
