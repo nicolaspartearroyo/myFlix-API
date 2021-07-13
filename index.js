a4468ff6ee7380d5a1e03af082c7d72a7837f19e
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-07-13 10:01:25 +0200

    index cors back to original idea

--- index.js
+++ index.js
@@ -22,16 +22,16 @@ app.use(bodyParser.json());
 let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflixbypartearroyo.herokuapp.com/'];
 
 app.use(cors(
-  // {
-  // origin: (origin, callback) => {
-  //   if (!origin) return callback(null, true);
-  //   if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-  //     let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-  //     return callback(new Error(message), false);
-  //   }
-  //   return callback(null, true);
-  // }
-  // }
+  {
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+        let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    }
+  }
 ));
 
 let auth = require('./auth.js')(app);
