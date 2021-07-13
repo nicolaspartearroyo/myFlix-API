1a02d4a142fef1fd12bfd4f3527037affd95dada
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-07-13 17:11:39 +0200

    birthdate typo fixed

--- models.js
+++ models.js
@@ -2,40 +2,40 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
-    Title: { type: String, required: true },
-    Description: { type: String, required: true },
-    Genre: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Genre'}],
-    Director: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Director' }],
-    Actors: [String],
-    ImagePath: String,
-    Featured: Boolean
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Genre' }],
+  Director: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Director' }],
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean
 });
 
 let userSchema = mongoose.Schema({
-    Username: { type: String, required: true },
-    Password: { type: String, required: true },
-    Email: { type: String, required: true },
-    Birthday: Date,
-    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthdate: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
 userSchema.statics.hashPassword = (password) => {
-    return bcrypt.hashSync(password, 10);
+  return bcrypt.hashSync(password, 10);
 };
 
 userSchema.methods.validatePassword = function (password) {
-    return bcrypt.compareSync(password, this.Password);
+  return bcrypt.compareSync(password, this.Password);
 };
 
 let directorSchema = mongoose.Schema({
-    Name: String,
-    Bio: String,
-    Birthdate: Date
+  Name: String,
+  Bio: String,
+  Birthdate: Date
 });
 
 let genreSchema = mongoose.Schema({
-    Name: String,
-    Description: String
+  Name: String,
+  Description: String
 });
 
 let Movie = mongoose.model('Movie', movieSchema);
